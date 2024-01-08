6fbd557338aa9cdeb877190b11d8db41f92c3bdf
MCausc78 <mcausc78@gmail.com>
2024-01-08 18:20:34 +0200

    launcher script

---
+++ launcher.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+i=0
+
+while [ $i -lt 3 ]; do
+  ./vbot
+  if [ $? -ne 0 ]; then
+    echo "Running bot failed, retrying in 3 seconds"
+    sleep 3
+    i=$(expr $i + 1)
+  else
+    i=0
+  fi
+done
+
+sleep 2592000
